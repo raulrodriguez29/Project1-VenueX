e0f5dd5f7e45cca67c96f2f0238e4fea68e01d6b
Raul Rodriguez <rodriguez29raul@gmail.com>
2026-01-02 12:44:02 -0600

    Initial backend setup with Spring Initializer

---
+++ backend/src/main/java/com/venuex/backend/VenueXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.venuex.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenueXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenueXBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/venuex/backend/VenueXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venuex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenueXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
