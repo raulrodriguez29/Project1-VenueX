3c026ec36de4a68b5766d8e20bf8e3836cb3b5ff
joshlian <jtl5655@mavs.uta.edu>
2026-01-08 12:39:05 -0600

    first branch commit

--- backend/src/main/java/com/venuex/backend/service/venueService.java
+++ backend/src/main/java/com/venuex/backend/service/venueService.java
@@ -1,4 +1,23 @@
 package com.venuex.backend.service;
 
+import com.venuex.backend.entities.venue;
+import com.venuex.backend.repository.venueRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class venueService {
+    private final venueRepository venuerepository;
+
+    public venueService (venueRepository venuerepository) {
+        this.venuerepository = venuerepository;
+    }
+
+    public List<venue> getAllVenues() {
+        return venuerepository.findAll();
+    }
+
+    public venue findById(Integer id) {
+        return venuerepository.findById(id).orElse(null);
+    }
 }
