d2b7f78f41b65d7a26cd82dee9bc14746ab35197
immafirinmalazer <kang.ashlee397@gmail.com>
2026-01-22 15:53:02 -0600

    added Blank.tsx and Page.tsx for template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,9 @@
-import Navbar from "./components/navbar/Navbar";
-import Hero from "./components/HeroSection";
-import TrendingEvents from "./components/TrendingEvents";
-import OrganizerPromo from "./components/OrganizerPromo";
-import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
     <div className="h-full w-full overflow-auto font-body bg-black text-white">
-      <Navbar />
-
-      {/* navbar height offset MUST remain */}
-      <main className="pt-16">
-        <Hero />
-        <TrendingEvents/>
-        <OrganizerPromo />
-      </main>
-
-      <Footer />
+      <Home/>
     </div>
   );
 }
---
+++ frontend/src/pages/Page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar/Navbar";
+import Blank from "../components/Blank";
+import Footer from "../components/Footer";
+
+export default function Page() {
+  return (
+    <>
+      <Navbar />
+    <Blank />
+      <Footer />
+    </>
+  )
+}
