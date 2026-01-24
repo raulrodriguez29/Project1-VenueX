cb98d071ac531cd394298ee25291bbc1b52eaed1
immafirinmalazer <kang.ashlee397@gmail.com>
2026-01-24 16:27:49 -0600

    edited import postfixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './auth/AuthContext.tsx'
+import { AuthProvider } from './auth/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
